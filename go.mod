9f92bdc97b0520b7f9972dc6330923adca4b84aa
Dejan Vasic <dvasic@seek.com.au>
2019-09-04 08:39:35 +1000

    feat(Database): Connecting to local postgres database

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/dejanvasic85/matchday-api
 
 go 1.12
 
-require gopkg.in/yaml.v2 v2.2.2
+require (
+	github.com/go-pg/pg v8.0.6+incompatible
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/lib/pq v1.2.0
+	gopkg.in/yaml.v2 v2.2.2
+	mellium.im/sasl v0.2.1 // indirect
+)
--- pkg/api/api.go
+++ pkg/api/api.go
@@ -3,10 +3,15 @@ package api
 import (
 	"fmt"
 	"github.com/dejanvasic85/matchday-api/pkg/utl/config"
+	"github.com/dejanvasic85/matchday-api/pkg/utl/database"
 )
 
 func Start(cfg *config.Configuration) error {
-	fmt.Println("Starting application server...")
+	fmt.Println("Opening Database connection...")
+	_, err := database.New(cfg.DB.PSN, cfg.DB.Timeout, cfg.DB.LogQueries)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
